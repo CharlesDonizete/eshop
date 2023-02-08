2953f5a4c4a471eb9ec36abb1ce3d11a68b8e4e4
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-02-08 17:24:05 -0300

    User Card

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { MonthlySalesChartComponent } from './monthly-sales-chart/monthly-sales-chart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
-  declarations: [LoadingComponent, MonthlySalesChartComponent],
+  declarations: [
+    LoadingComponent,
+    MonthlySalesChartComponent,
+    NavbarComponent,
+    UserCardComponent,
+  ],
   imports: [CommonModule, IonicModule],
-  exports: [LoadingComponent, MonthlySalesChartComponent],
+  exports: [
+    LoadingComponent,
+    MonthlySalesChartComponent,
+    NavbarComponent,
+    UserCardComponent,
+  ],
 })
 export class ComponentsModule {}
